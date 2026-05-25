3c101ef5d3c04393d359e1736a9cca1e1fd427fb
Vishnu Madhani <madhanivishnu2004@gmail.com>
2026-05-25 17:50:25 +0530

    feat: add CartId to Order entity and update related configurations

---
+++ WMS.Infrastructure/Migrations/20260525121848_AddCartIdInOrder.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCartIdInOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CartId",
+                table: "orders",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "orders_cart_id_idx",
+                table: "orders",
+                column: "CartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_carts_CartId",
+                table: "orders",
+                column: "CartId",
+                principalTable: "carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_carts_CartId",
+                table: "orders");
+
+            migrationBuilder.DropIndex(
+                name: "orders_cart_id_idx",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "orders");
+        }
+    }
+}
