d9698af204ba52d1420656979de2ea11c3c947a2
Vishnu Madhani <madhanivishnu2004@gmail.com>
2026-06-02 10:25:17 +0530

    refactor: remove Draft status and Notes property from Shipment entity

---
+++ WMS.Infrastructure/Migrations/20260602045223_ShipmentChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShipmentChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "shipments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "shipments",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
